dc0e892989f8db77bbdb0c6955b4daf7be77a492
syrene bartolome <syrene.h.bartolome@icloud.com>
2024-01-04 20:23:19 +0900

    created routes, controller, views

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,19 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    compute_answer(@question)
+  end
+
+  def compute_answer(question)
+    if question == 'I am going to work'
+      @answer = 'Great!'
+    elsif question.end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and got to work!"
+    end
+  end
+end
